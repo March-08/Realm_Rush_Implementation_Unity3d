554bc3faea5c08831c24c4972c77636f58120fc9
mpoliti <March08>
2020-06-29 23:02:44 +0200

    Setting up towers Queue

--- Assets/Scripts/TowerFactory.cs
+++ Assets/Scripts/TowerFactory.cs
@@ -6,27 +6,38 @@ public class TowerFactory : MonoBehaviour{
 
     [SerializeField] Tower towerPrefab;
     [SerializeField] int towerLimit=5;
+    Queue<Tower> towerQueue = new Queue<Tower>();
+
+    public Waypoint baseWaypoint;   //where the tower is standing on
 
     int numTowers=0;
 
     public void AddTower(Waypoint baseWaypoint) {
-        if (numTowers < towerLimit) {
+
+        int numTowers = towerQueue.Count;
+
+        if (numTowers<towerLimit) {
             InstantiateNewTower(baseWaypoint);
         }
         else {
-            MoveExistingTower();    // todo move!
+            MoveExistingTower(baseWaypoint);    // todo move!
         }
 
     }
 
-    private static void MoveExistingTower() {
-        print("limit reached");
+    private  void MoveExistingTower(Waypoint baseWaypoint) {
+        var oldTower = towerQueue.Dequeue();
+
+        towerQueue.Enqueue(oldTower);
+
     }
 
     private void InstantiateNewTower(Waypoint baseWaypoint) {
-        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        var newTower=Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
         baseWaypoint.isPlaceable = false;
         numTowers += 1;
+        towerQueue.Enqueue(newTower);
+
     }
 
 
